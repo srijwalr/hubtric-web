502f341bb22605cd923a21de1eb9759812a04fd6
Srijwal R <srijwal6@gmail.com>
2024-07-20 00:10:14 +0530

    Footer done, Contact us page color corrections

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -1,54 +1,95 @@
-import Hubtric from '@/assets/icons/Hubtric.png';
-import { routes } from '@/lib/constants';
-import { Link } from 'react-router-dom';
+import Hubtric from "@/assets/icons/Hubtric.png";
+import { routes } from "@/lib/constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
+  const footerLinks = [
+    {
+      heading: "Quick Links",
+      links: [
+        { name: "Home", path: routes.home() },
+        { name: "About Us", path: routes.aboutUs() },
+        { name: "Capabilities", path: routes.capabilities() },
+        { name: "Industries", path: routes.industries() },
+      ],
+    },
+    {
+      heading: "Capabilities",
+      links: [
+        { name: "Precision CNC Machining", path: routes.capabilities() },
+        { name: "Advanced Tooling Solutions", path: routes.capabilities() },
+        { name: "Injection Molding", path: routes.capabilities() },
+        { name: "Sub Assembly Service", path: routes.capabilities() },
+        { name: "Design and Prototyping", path: routes.capabilities() },
+        { name: "Research and Development", path: routes.capabilities() },
+      ],
+    },
+  ];
+
+  const socialLinks = [
+    {
+      icon: faPhone,
+      items: [
+        { path: "tel:+91-7899176414", value: "+91 7899176414" },
+        // { path: 'tel:+49-15772902328', value: '+49 15772902328'},
+      ],
+    },
+    {
+      icon: faWhatsapp,
+      items: [{ path: "https://wa.me/919901392219", value: "+91 9901392219" }],
+    },
+    {
+      icon: faEnvelope,
+      items: [
+        { path: "mailto:info@hubtric.com", value: "info@hubtric.com" },
+        { path: "mailto:sales@hubtric.com", value: "sales@hubtric.com" },
+      ],
+    },
+  ];
+
   return (
-    <footer className='bg-secondary w-full'>
-      <div className='container grid md:grid-cols-5 grid-cols-1 py-8'>
-        <div className='col-span-2 flex flex-col md:flex-row items-baseline md:items-center md:justify-center gap-8'>
-          <img className='w-24' src={Hubtric} alt='' />
-          <div className='flex flex-col items-start gap-4 justify-center text-xs'>
-            <div className='flex items-start gap-1 text-primary'>
-              <span className='font-bold'>Tel.</span>
-              <div className='flex flex-col'>
-                <Link to='tel:+91-7899176414'>+91 7899176414</Link>
-                <Link to='tel:+49-15772902328'>+49 15772902328</Link>
-              </div>
-            </div>
-            <div className='flex items-start gap-1 text-primary'>
-              <span className='font-bold'>WA.</span>
-              <div className='flex flex-col'>
-                <Link to='https://wa.me/919901392219'>+91 9901392219</Link>
-              </div>
-            </div>
-            <div className='flex items-start gap-1 text-primary'>
-              <span className='font-bold'>Mail.</span>
-              <div className='flex flex-col'>
-                <Link to='mailto:info@hubtric.com'>info@hubtric.com</Link>
-                <Link to='mailto:sales@hubtric.com'>sales@hubtric.com</Link>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='col-span-3'></div>
+    <footer className="bg-secondary text-white py-4 px-6">
+      
+
+      <div className="flex flex-wrap justify-even text-xs text-secondary-foreground font-medium gap-8">
+      <div className="flex-1">
+        <img className="w-24" src={Hubtric} alt="" />
       </div>
-      <div className='p-6 bg-black text-white text-xs'>
-        <div className='container'>
-          <p>
-            Copyrights © 2024{' '}
-            <Link
-              className='hover:text-primary transition-colors'
-              to={routes.home()}
-            >
-              Hubtric Manufacturing Pvt. Ltd.
-            </Link>{' '}
-            All rights reserved
-          </p>
+        {footerLinks.map((linkSection) => (
+          <div className="w-full md:w-auto flex-1">
+            <h2 className="text-lg font-semibold">{linkSection.heading}</h2>
+            <ul className="flex flex-col gap-1 mt-1">
+              {linkSection.links.map((link) => (
+                <li key={link.name}>
+                  <Link to={link.path} className="hover:text-primary block">
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+        <div className="w-full md:w-auto flex-1">
+        <h2 className="text-lg font-semibold">Contact Us</h2>
+        {socialLinks.map((link) => (
+            <div className="flex items-start gap-2 mt-1">
+              <span className="font-bold">
+                <FontAwesomeIcon icon={link.icon} />
+              </span>
+              <div className="flex flex-col">
+                {link.items.map((contact) => (
+                  <Link to={contact.path} className="hover:text-primary block">{contact.value}</Link>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
--- src/pages/about/components/ContactUs.tsx
+++ src/pages/about/components/ContactUs.tsx
@@ -6,11 +6,11 @@ const ContactUs = () => {
     return(
         
       <section className="p-0">
-      <div className="flex flex-col md:flex-row items-center bg-gray-900 text-white relative p-8 pl-14 pt-14">
+      <div className="flex flex-col md:flex-row items-center bg-black text-primary-foreground relative p-8 pl-14 pt-14">
       {/* <div className="bg-white absolute w-1/5 z-0 -inset-1 hidden md:block"></div> */}
 
       <div className="relative mb-8 md:mb-0">
-        <div className="bg-orange-600 absolute inset-0 transform -translate-x-6 -translate-y-6"></div>
+        <div className="bg-primary absolute inset-0 transform -translate-x-6 -translate-y-6"></div>
         <img
           src={ContactUsImg}
           alt="People discussing a project"
@@ -19,12 +19,12 @@ const ContactUs = () => {
       </div>
       <div className="md:ml-12 text-center md:text-left">
         <h2 className="text-3xl font-bold mb-4">Dive Deeper</h2>
-        <p className="text-gray-400 mb-4">
+        <p className="text-primary-foreground mb-4">
         We're here to help! Contact us to collaborate on your next project and discover how our 
         precision machining and manufacturing excellence can elevate your industry.
         </p>
         <Link to={routes.contactUs()}
-          className="inline-block bg-gray-800 text-white py-2 px-4 border border-white hover:bg-white hover:text-gray-900 uppercase"
+          className="inline-block bg-black text-primary-foreground py-2 px-4 border border-primary-foreground hover:bg-primary-foreground hover:text-secondary-foreground uppercase"
         >
           Request demo
         </Link>
